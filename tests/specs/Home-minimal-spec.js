0f0d0a610953680dd272979d6afeb71a2ee9d8df
Valentina <va.ruizi@duocuc.cl>
2025-10-20 23:48:44 -0300

    prueba simple

---
+++ tests/specs/Home-minimal-spec.js
@@ -0,0 +1,6 @@
+// tests/specs/Home-minimal.spec.js
+describe('Home - Pruebas Mínimas', function() {
+    it('debería cargar sin errores', function() {
+        expect(true).toBe(true);
+    });
+});
